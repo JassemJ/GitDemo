be28e269150b4114dd6cd3d4750d4b948bc2ec93
jassem <jassem.jahangir01@gmail.com>
2022-04-01 12:45:41 -0400

    commit random

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -5,11 +5,11 @@ import org.testng.annotations.Test;
 public class AppiumTest {
     @Test
     public void AndroidApps(){
-        System.out.println("android");
+        System.out.println("android1");
     }
 
     @Test
     public void IOSApps(){
         System.out.println("ios");
     }
-}a~QQQ
+}
